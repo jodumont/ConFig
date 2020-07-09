4a14778c28e1e146fdbe4da9d7ab47c7d1f18858
Jonathan Dumont <5204724+JOduMonT@users.noreply.github.com>
2020-07-09 09:44:41 +0700

    add prefetch
    https://gist.github.com/swsalim/52125e3f2f10323b4db879a0d8b479ee

--- wordpress/function.php
+++ wordpress/function.php
@@ -60,3 +60,26 @@ add_action('wp_enqueue_scripts', 'wpdocs_dequeue_dashicon');
 // Disable Contact Form 7 CSS/JS on Every Page
 add_filter('wpcf7_load_js', '__return_false');
 add_filter('wpcf7_load_css', '__return_false');
+
+//* Adding DNS Prefetching
+function wp_dns_prefetch() {
+echo '<meta http-equiv="x-dns-prefetch-control" content="on">
+<link rel="dns-prefetch" href="//fonts.googleapis.com" />
+<link rel="dns-prefetch" href="//fonts.gstatic.com" />
+<link rel="dns-prefetch" href="//ajax.googleapis.com" />
+<link rel="dns-prefetch" href="//apis.google.com" />
+<link rel="dns-prefetch" href="//google-analytics.com" />
+<link rel="dns-prefetch" href="//www.google-analytics.com" />
+<link rel="dns-prefetch" href="//ssl.google-analytics.com" />
+<link rel="dns-prefetch" href="//youtube.com" />
+<link rel="dns-prefetch" href="//api.pinterest.com" />
+<link rel="dns-prefetch" href="//connect.facebook.net" />
+<link rel="dns-prefetch" href="//platform.twitter.com" />
+<link rel="dns-prefetch" href="//syndication.twitter.com" />
+<link rel="dns-prefetch" href="//syndication.twitter.com" />
+<link rel="dns-prefetch" href="//platform.instagram.com" />
+<link rel="dns-prefetch" href="//s.gravatar.com" />
+<link rel="dns-prefetch" href="//s0.wp.com" />
+<link rel="dns-prefetch" href="//stats.wp.com" />';
+}
+add_action('wp_head', 'wp_dns_prefetch', 0);
